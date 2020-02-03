674bfc872924d8f1bd6377a5383d2a70f4d283ab
Rodrigo Antonio da Cruz <52705593+RodrigoAntonioCruz@users.noreply.github.com>
2020-02-02 21:54:05 -0300

    Update senha.blade.php

--- src/resources/views/auth/redefinicao/senha.blade.php
+++ src/resources/views/auth/redefinicao/senha.blade.php
@@ -52,7 +52,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
+                                    {{ __('Alterar Senha') }}
                                 </button>
                             </div>
                         </div>
